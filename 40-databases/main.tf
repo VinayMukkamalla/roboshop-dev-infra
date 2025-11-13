973aba69d4970b23b409d42c4ba630a2248b87f3
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-11-13 12:11:14 +0530

    repo integration with new changes

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -40,3 +40,47 @@ resource "terraform_data" "mongodb" {
      ]
   }
 }
+
+
+resource "aws_instance" "mongodb" {
+  ami = local.ami
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.mongodb_sg_id]
+  subnet_id = local.subnet_id
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-mongodb"
+    }
+
+  )
+
+}
+
+
+resource "terraform_data" "redis" {
+  
+  triggers_replace = [
+    aws_instance.redis.id
+  ]
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.redis.private_ip
+  }
+
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh redis"
+
+     ]
+  }
+}
