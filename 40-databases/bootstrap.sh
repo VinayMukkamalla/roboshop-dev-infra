d2cc2e780fa7ac810dfdccf0d1934863004aafeb
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-11-13 11:12:26 +0530

    repo integration with new changes

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,6 +2,5 @@
 component = $1
 
 dnf install ansible -y
-ansible-pull -U https://github.com/VinayMukkamalla/ansible-roboshop-roles-tf.git -e component = $1 main.yaml -e "component=mongodb"
+ansible-pull -U https://github.com/VinayMukkamalla/ansible-roboshop-roles-tf.git -e component = $1 main.yaml 
 
-#
