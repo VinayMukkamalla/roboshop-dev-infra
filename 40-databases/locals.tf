6010e2e95a563e2f4f6b6986667187f9ac7949d6
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-11-13 10:54:17 +0530

    repo integration with new changes

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -2,7 +2,7 @@ locals {
   common_name_suffix = "${var.project_name}-${var.environment}"
   mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value 
   ami = data.aws_ami.aminame.id
-  subnet_id = data.aws_ssm_parameter.database_subnet_ids.value[0]
+  subnet_id = data.aws_ssm_parameter.database_subnet_ids[0]
 
   common_tags = {
 
