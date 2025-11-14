c9d5bdcb02aa5b87cfc3c5ec25a7790f7a4ff80b
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-11-14 11:03:33 +0530

    repo integration with new changes

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,5 +2,27 @@
 component=$1
 
 dnf install ansible -y
-ansible-pull -U https://github.com/VinayMukkamalla/ansible-roboshop-roles-tf.git -e component=$1 main.yaml 
+# ansible-pull -U https://github.com/VinayMukkamalla/ansible-roboshop-roles-tf.git -e component=$1 main.yaml 
+# as ansible-pull is not respecting our inventory.ini we are using alternative way through shell scripting
+
+REPO_URL=https://github.com/VinayMukkamalla/ansible-roboshop-roles-tf.git
+ANSIBLE_DIR=/opt/roboshop/ansible
+REPO_DIR=ansible-roboshop-roles-tf
+
+mkdir -p /var/log/ansible
+cd /var/log/ansible
+touch ansible.log
+
+mkdir -p $ANSIBLE_DIR
+cd $ANSIBLE_DIR
+
+if [ -d $REPO_DIR ]; then
+    cd $$REPO_DIR
+    git pull
+else
+    git clone $REPO_URL
+    cd $REPO_DIR
+fi
+
+ansible-playbook -e component=$component main.yaml
 
