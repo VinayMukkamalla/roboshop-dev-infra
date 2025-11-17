9806c13d8412f2426e620a0e4c56d4479d900b1c
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-11-17 14:39:53 +0530

    repo integration with new changes

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -65,3 +65,21 @@
   protocol = "tcp"
   to_port = 22
  }
+
+   resource "aws_security_group_rule" "bastion_catalogue" {
+  type = "ingress"
+  security_group_id = local.catalogue_sg_id  # catalogue_sg_id 
+  source_security_group_id  = local.bastion_sg_id
+  from_port = 22
+  protocol = "tcp"
+  to_port = 22
+ }
+
+   resource "aws_security_group_rule" "catalogue_mongodb" {
+  type = "ingress"
+  security_group_id = local.mongodb_sg_id  # mongodb sg_id 
+  source_security_group_id  = local.catalogue_sg_id
+  from_port = 22
+  protocol = "tcp"
+  to_port = 22
+ }
