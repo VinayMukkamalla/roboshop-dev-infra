566d0d9bfdf6546c3a9cd116a58cd4c36b05d3d1
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-11-17 14:17:55 +0530

    repo integration with new changes

---
+++ 60-catalogue/locals.tf
@@ -0,0 +1,12 @@
+locals {
+  ami_id = data.aws_ami.aminame.id
+  catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id
+  private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  common_name_suffix = "${var.project_name}-${var.environment}"
+
+  common_tags = {
+    Project = var.project_name
+    environment = var.environment
+    Terraform = "true"
+  }
+}
---
+++ 60-catalogue/main.tf
@@ -0,0 +1,60 @@
+resource "aws_instance" "catalogue" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.catalogue_sg_id]
+  subnet_id = local.private_subnet_id
+  
+  tags = merge(
+        local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-catalogue"
+    }
+   )
+}
+
+
+resource "terraform_data" "catalogue" {
+  
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.catalogue.private_ip
+  }
+
+  provisioner "file" {
+    source = "catalogue.sh"
+    destination = "/tmp/catalogue.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/catalogue.sh",
+        "sudo sh /tmp/catalogue.sh catalogue ${var.environment}"
+     ]
+  }
+}
+
+resource "aws_ec2_instance_state" "catalogue" {
+  instance_id = aws_instance.catalogue.id
+  state       = "stopped"
+  depends_on = [ terraform_data.catalogue ]
+}
+
+
+resource "aws_ami_from_instance" "catalogue_ami" {
+  source_instance_id = aws_instance.catalogue.id
+  name               = "${local.common_name_suffix}-catalogue-AMI"
+  description        = "AMI created from MySourceInstance" 
+  tags = merge(
+        local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-catalogue-AMI"
+    }
+   ) 
+  depends_on = [ aws_ec2_instance_state.catalogue ]
+}
