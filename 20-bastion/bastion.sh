6cb199e40026fc16297a76c5f926969f07fa0465
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-11-17 14:41:12 +0530

    repo integration with new changes

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -3,3 +3,16 @@
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
+
+
+
+
+
+
+
+##########
+# sudo growpart /dev/nvme0n1 4 -> increase the partition
+# sudo lsblk -> check once again
+# sudo lvextend -L +30G /dev/mapper/RootVG-homeVol
+# sudo xfs_growfs /home
+##########
