c61b7a250673bbe7a56c642cc8ad5c8ce9ca22ff
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-11-18 19:02:16 +0530

    repo integration with new changes

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -2,6 +2,8 @@ locals {
   ami_id = data.aws_ami.aminame.id
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  private_subnet_id_2 = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[1]
+  vpc_id = data.aws_ssm_parameter.vpc_id
   common_name_suffix = "${var.project_name}-${var.environment}"
 
   common_tags = {
