627726d03c5bd22366678f2736fab1a69211a50e
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-11-18 23:49:43 +0530

    repo integration with new changes

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -3,7 +3,7 @@ locals {
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
   private_subnet_id_2 = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[1]
-  vpc_id = data.aws_ssm_parameter.vpc_id
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
   common_name_suffix = "${var.project_name}-${var.environment}"
 
