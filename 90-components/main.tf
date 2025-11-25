25e7f6467339c9a3c1a7ff860150004ea2316492
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-11-25 15:11:26 +0530

    repo integration with new changes

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,5 +1,13 @@
+# module "components" {
+#   source = "../../terraform-roboshop-component"
+#   component = var.component
+#   rule_priority = var.rule_priority
+# }
+
+
 module "components" {
-  source = "../../terraform-roboshop-component"
-  component = var.component
-  rule_priority = var.rule_priority
+  for_each = var.components
+  source = "git::https://github.com/VinayMukkamalla/terraform-roboshop-component.git?ref=main"
+  component = each.key
+  rule_priority = each.value.rule_priority
 }
