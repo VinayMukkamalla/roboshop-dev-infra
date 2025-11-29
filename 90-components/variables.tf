f11fa053f98cc959d10b3d45e871c273a31b8aaf
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-11-29 18:43:53 +0530

    repo integration with new changes

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -10,22 +10,21 @@ variable "rule_priority" {
 
 variable "components" {
   default = {
-    
     frontend = {
         rule_priority = 10
     }
-    catalogue = {
-        rule_priority = 10
-    }
-    user = {
-        rule_priority = 20
-    }
-    cart = {
-        rule_priority = 30
-    }
-    shipping = {
-        rule_priority = 40
-    }
+    # catalogue = {
+    #     rule_priority = 10
+    # }
+    # user = {
+    #     rule_priority = 20
+    # }
+    # cart = {
+    #     rule_priority = 30
+    # }
+    # shipping = {
+    #     rule_priority = 40
+    # }
     payment = {
         rule_priority = 50
     }
